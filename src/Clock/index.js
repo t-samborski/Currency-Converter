18bc5022729c8d7739f8ce4b9c6a198ff37f3332
Tomasz Samborski <tomek986@wp.pl>
2024-03-11 13:09:55 +0100

    create custom Hook and add clock to him

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,25 +1,14 @@
 import { TimePlace } from "./styled"
-import { useEffect, useState } from "react";
+import { useCurrentClock } from "./useClock"
 
 
 export const Clock = () => {
 
-    const [date, setDate] = useState(new Date())
-
-    useEffect(() => {
-       const intervalId = setInterval(() => {
-            setDate(new Date())
-        }, 1000);
-
-            return () =>{
-                clearInterval(intervalId)
-            }
-        
-    }, [date])
+    const date = useCurrentClock()
 
     return (
 
-        <TimePlace>Aktualny czas: {date.toLocaleString()}</TimePlace>
+        <TimePlace> Aktualny czas: {date.toLocaleString()} </TimePlace>
 
     )
 }
---
+++ src/Clock/useClock.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useCurrentClock = () => {
+    
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+       const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000);
+
+            return () =>{
+                clearInterval(intervalId)
+            }
+        
+    }, [date])
+
+    return date
+}
